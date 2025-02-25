9b1636f6cc012c08bb91a51befe0747bc3bcc257
Mario_Brnal10 <145825327+MarioBrnal10@users.noreply.github.com>
2025-02-24 18:38:45 -0600

    segundo commit

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="db"
+    )
+    cursor = conn.cursor()  # Crear el cursor correctamente
+    cursor.execute("SELECT * FROM students")  # Ejecutar la consulta correctamente
+    students = cursor.fetchall()  # Obtener los resultados
 
+    cursor.close()  # Cerrar el cursor después de usarlo
+    conn.close()  # Cerrar la conexión después de usarla
+
+    return render_template('index.html', students=students)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
